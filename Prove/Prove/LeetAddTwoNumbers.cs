920df867fe7ff5ef95ee8515592f3bada9e8d3d4
mcauzzi <marcosfurai93>
2019-04-03 15:47:51 +0200

    Primo Commit con somma di due numeri

---
+++ Prove/Prove/LeetAddTwoNumbers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prove
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+    class LeetAddTwoNumbers
+    {
+       
+
+        public static ListNode addTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode temp = l1 ?? l2;
+            int carry = 0;
+
+            while ((l1 != null && l2!=null)||carry>0)
+            {
+                
+                l1.val = (l1?.val??0) + (l2?.val??0) + carry;
+                if (l1.val > 9)
+                {
+                    carry = l1.val/10;
+                    l1.val -= 10;
+                }
+                else
+                {
+                    carry = 0;
+                }
+                if (l1.next == null && (l2?.next != null || carry > 0))
+                {
+                    l1.next = new ListNode(0);
+                }
+                l1 = l1.next;
+                l2 = l2?.next;
+            }
+            return temp;
+        }
+
+        private int GetLength(ListNode l)
+        {
+            int len = 0;
+            while (l != null)
+            {
+                len++;
+            }
+
+            return len;
+        }
+
+    }
+}
---
+++ Prove/Prove/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Prove
+{
+    class Program
+    {
+        /// <summary>
+        /// Main Method
+        /// </summary>
+        /// <param name="args">Main args</param>
+        private static void Main(string[] args)
+        {
+            ListNode l1 = new ListNode(5);
+            //l1.next = new ListNode(4);
+            //l1.next.next = new ListNode(3);
+
+            ListNode l2 = new ListNode(5);
+            //l2.next = new ListNode(6);
+            //l2.next.next = new ListNode(4);
+
+            var l=LeetAddTwoNumbers.addTwoNumbers(l1, l2);
+
+            Console.WriteLine(l.val);
+        }
+    }
+}
